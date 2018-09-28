12160d534a8f5e92e40b3b50df2a2162deb72275
ChristopheRibault <c.ribault.dev@gmail.com>
2018-09-28 13:13:50 +0200

    Modification des composants en class au lieu de function

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './Search.css';
 
-const Search = () => (
-	<main>
-		<form>
-			<label htmlFor="searchInput"></label>
-			<div className="inputs">
-				<input type="text" id="searchInput" placeholder="Search movie..."/>
-				<input type="submit" value="Search"/>
-			</div>
-		</form>
-		<p className="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio autem eum rerum pariatur, id a minima, doloribus natus nihil fuga alias ut molestiae illum neque! Vero mollitia modi alias esse.</p>
-	</main>
-);
+class Search extends Component {
+	render(){
+		return(
+			<main>
+				<form>
+					<label htmlFor="searchInput"></label>
+					<div className="inputs">
+						<input type="text" id="searchInput" placeholder="Search movie..."/>
+						<input type="submit" value="Search"/>
+					</div>
+				</form>
+				<p className="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio autem eum rerum pariatur, id a minima, doloribus natus nihil fuga alias ut molestiae illum neque! Vero mollitia modi alias esse.</p>
+			</main>
+		);
+	}
+}
 
 export default Search;
