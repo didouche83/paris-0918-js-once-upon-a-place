6919e8de05322599af1bfbed3891d25122706237
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-03 12:34:12 +0200

    ajout d'un switch dans page resultats

--- src/Results/ResultList.js
+++ src/Results/ResultList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class ResultList extends Component {
     render() {
+        console.log(this.props.locationsList)
         return(
         <p>ResultList</p>
         );
