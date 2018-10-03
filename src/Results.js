7c68c8f8c6074f6af39e0572c8ed63e5436a7aed
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-03 11:55:10 +0200

    appel composant resultList

--- src/Results.js
+++ src/Results.js
@@ -1,46 +1,49 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
-import List from './List';
-import Map from './Map';
-import HeaderResults from './HeaderResults';
+import ResultList from './Results/ResultList';
+// import Map from './Map';
+// import HeaderResults from './HeaderResults';
 
 class Results extends Component {
   state = {
-    title: undefined,
-    writer: undefined,
-    release_year: undefined,
-    locations: undefined
+    res: []
+  };
+
+  searchMovie = async () =>{
+    const api_call = await fetch(`https://data.sfgov.org/resource/wwmu-gmzc.json?$where=title like '%25Man%25'`);
+    if (api_call.ok) {
+      const data = await api_call.json();
+      this.setState({
+        res: data
+      })
+    }else{
+      const data = undefined;
+    }
   };
 
   componentWillMount(){
-    searchMovie = async (e) =>{
-      const api_call = await fetch(`https://data.sfgov.org/resource/wwmu-gmzc.json?$where=title like '%25Man%25'`);
-      if (api_call.ok) {
-          const data = await api_call.json();
-          console.log(data);
-      }else{
-        data = undefined;
-      }
-    };
+    this.searchMovie()
   };
-
 
   render(){
     return(
       <div className= "Results">
-        <HeaderResults/>
+        {/* <HeaderResults/> */}
 
-        <BrowserRouter className="mobileOnly">
+        {/* <BrowserRouter className="mobileOnly">
           <NavLink to="/Result/List">List</NavLink>
           <NavLink to="/Result/Map">Map</NavLink>
           <Switch>
-            <Route path="/Result/List" component={List} />
+            <Route path="/Result/List" component={ResultList} />
             <Route path="/Results/Map" component={Map} />
           </Switch>
-        </BrowserRouter>
+        </BrowserRouter> */}
 
-        <List className="desktopOnly"/>
-        <Map className="desktopOnly"/>
+        <ResultList 
+          className="desktopOnly"
+          locationsList = {this.state.res}
+        />
+        {/* <Map className="desktopOnly"/> */}
       </div>
     );
   }
