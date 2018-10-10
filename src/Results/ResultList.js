8de1056ad35b6d9714c9efc79b95f4315343966f
David Huveau <d.huveau@gmail.com>
2018-10-10 17:38:45 +0200

    ajout de la fonciton pour adapater le Json

--- src/Results/ResultList.js
+++ src/Results/ResultList.js
@@ -3,12 +3,32 @@ import MediaCard from './MediaCard';
 import './ResultList.css';
 
 class ResultList extends Component {
+
+    transformDatas = datas => {
+        let res = [];
+        datas.map(data => {
+
+            const film = res.filter(f => f.title === data.title && f.release_year === data.release_year);
+            if (!film.length) {
+                const add = {};
+                add.title = data.title;
+                add.release_year = data.release_year;
+                add.locations = new Array(data.locations);
+                res.push(add);
+            } else {
+                res.filter(f => f.title === data.title && f.release_year === data.release_year)[0].locations.push(data.locations);
+            }
+
+        })
+        return res;
+    };
+
     render() {
-         console.log("resultList",this.props.locationsList);
-        
+        let datas = this.transformDatas(this.props.locationsList);
+        // console.log("resultList", datas);
         return (
             <div className='cardContainer'>{
-                this.props.locationsList.map(e => {
+                datas.map(e => {
                     //return <p>Resultat: {e.title}</p>
                     return (
                         <div className='card'>
